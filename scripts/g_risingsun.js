fca6705ba7285d447fce3b67bf697221e69e8e8b
Tomas Vitasek <tomas.vitasek@rl.cz>
2019-04-18 13:41:46 +0200

    All javascript funcitonality for Rising Sun

--- scripts/g_risingsun.js
+++ scripts/g_risingsun.js
@@ -2,32 +2,70 @@
 'use strict';
 
 // Rising sun global variables
-var g_clans     = ['a', 'b'], // [TODO] - RS - list of clans
-    g_exp_clans = ['c', 'd']; // [TODO] - RS - list of expansion clans
-
-    // Dynasty Invasion (3-6) - Sun, Moon
-    // Daimyo Box (3-6) - Fox
-    // Rising Sun (3-5) - Koi, Turtle, Bonsai, Lotus, Dragonfly
+var g_clans     = ['bonsai', 'dragonfly', 'koi', 'lotus', 'turtle'],
+    g_dai_clans = ['fox'],
+    g_exp_clans = ['moon', 'sun'];
 
 // Register the action on the shuffleClans button
 document.getElementById('shuffleClans').addEventListener('click', function() {
-  var numberOfPlayers = document.getElementById('numberOfPlayers').value,
+  var numberOfPlayers = Number(document.getElementById('numberOfPlayers').value),
+      daimyo          = document.getElementById('daimyoBox').checked,
       expansion       = document.getElementById('dynastyExpansion').checked,
-      clansArray      = g_clans;
+      clansArray      = g_clans,
+      playersCard     = document.getElementById('rs-clans'),
+      playersSlider   = document.getElementById('rs-clans-slider');
 
-  // Add Expansion if checked
-  if (expansion) {
-    clansArray = clansArray.concat(g_exp_clans);
+  // Six players available only with Daimyo Box or Dynasty Expansion (or both)
+  if (!daimyo && !expansion && numberOfPlayers === 6) {
+    app.createConfirm('','Six players game is available only with Daimyo Box or Dynasty Expansion (or both).');
+  } else {
+
+    // Add Daimyo Box clan if checked
+    if (daimyo) {
+      clansArray = clansArray.concat(g_dai_clans);
+    }
+
+    // Add Expansion clans if checked
+    if (expansion) {
+      clansArray = clansArray.concat(g_exp_clans);
+    }
+
+    // Shuffle the clans
+    clansArray = app.arrayShuffle(clansArray);
+
+    // Clear previous results
+    while (playersSlider.hasChildNodes()) {   
+      playersSlider.removeChild(playersSlider.firstChild);
+    }
+
+    // Display the clans for players
+    for (var i = 0; i < numberOfPlayers; i++) {
+
+      // New elements
+      var slide = document.createElement('div'),
+          title = document.createElement('div'),
+          image = document.createElement('img');
+
+      // Setting the elements
+      slide.className = 'cardSlide';
+      title.className = 'cardSlideTitle';
+      title.innerText = 'Player ' + (i + 1);
+      image.src       = 'images/games/risingsun/' + clansArray[i] + '.png';
+      image.alt       = clansArray[i];
+
+      // Append the elements
+      slide.appendChild(title);
+      slide.appendChild(image);
+      playersSlider.appendChild(slide);
+
+    }
+
+    // Display the players card
+    playersCard.hidden = false;
+
   }
 
-  // Shuffle the clans
-  clansArray = app.arrayShuffle(clansArray);
-
-  // Display the clans for players
-  console.log('next :: ' + clansArray.join(',')); // [TODO] - RS - Displaying the list of clans for set amount of users
-
 });
-
 
 // On load init
 document.getElementById('rs-clans').hidden = true;
